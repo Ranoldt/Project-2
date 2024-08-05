e91da9f405df93599200aeb05ad6cb1218a283db
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-08-05 14:32:28 -0700

    class def and init

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -1,7 +1,11 @@
 import keyword
 
 def mynamedtuple(type_name, field_names, mutable = False, default = {}):
-    assert type(type_name) is str and type_name[0].isalpha() == True and keyword.iskeyword(type_name) == False
+    try:
+        assert type(type_name) is str and type_name[0].isalpha() == True and keyword.iskeyword(type_name) == False
+    except AssertionError:
+        raise SyntaxError(f"Invalid type name: {type_name}")
+
     if type(field_names) is list:
         pass
     elif type(field_names) is str:
@@ -12,10 +16,17 @@ def mynamedtuple(type_name, field_names, mutable = False, default = {}):
     else:
         raise SyntaxError("Fields must be a list or a string")
 
-    for name in field_names:
-        assert name[0].isalpha() == True and keyword.iskeyword(name) == False
+    try:
+        for name in field_names:
+            assert name[0].isalpha() == True and keyword.iskeyword(name) == False
+    except AssertionError:
+        raise SyntaxError(f"Invalid field name: {name}")
 
     field_list = [element for element in field_names if element in list(default)]
     if len(field_list) != len(list(default)):
-        raise SyntaxError("")
+        raise SyntaxError("Mismatch between default fields and provided fields")
 
+    class_name = f'class {type_name}:\n'
+    init_method = f'    def init(self, {",".join(f'{name}= {value}' for name in field_names for value in default[name])}):\n'
+    for name in field_names:
+        init_method += f'        self.{name} = {name}\n'
