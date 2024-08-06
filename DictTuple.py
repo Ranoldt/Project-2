5ba99be57deebdfcdc6632791589738138f783c3
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-08-06 12:27:39 -0700

    DictTuple (repr, contains)

--- DictTuple.py
+++ DictTuple.py
@@ -17,9 +17,21 @@ class DictTuple:
             return True
         return False
 
+    def __repr__(self):
+        return f'{self.__class__.__name__}({", ".join(f'{item}' for item in self.dt)})'
+
+    def __contains__(self, item):
+        keys_set = {key for item in self.dt for key in item.keys()}
+        return item in keys_set
+
 
 if __name__ == '__main__':
     coordinate = mynamedtuple('coordinate', 'x y')
     d = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
     print(len(d))
     print(bool(d))
+    print(d)
+    if 'c2' in d:
+        print('yea')
+    else:
+        print('no')
